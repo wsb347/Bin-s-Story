07c8118e99918c0cf3e4b920ed5134448ec3a884
wsb347 <wsb347@naver.com>
2024-08-27 19:34:55 +0900

    setting

---
+++ src/main/java/com/example/binsstory/BinsStoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.binsstory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BinsStoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BinsStoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/binsstory/BinsStoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.binsstory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BinsStoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
